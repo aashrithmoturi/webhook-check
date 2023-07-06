602a7657f054ac0551a0547a9c00584d83bbd59f
Aashrith Moturi <Aashrith.Moturi@walmart.com>
2023-07-06 11:47:59 +0530

    testing function

--- validation/validate_test.go
+++ validation/validate_test.go
@@ -2,7 +2,7 @@ package validation
 
 import "testing"
 
-func Test_validateEmail(t *testing.T) {
+func Test_validateOwner(t *testing.T) {
 	type args struct {
 		file string
 		s    string
@@ -13,14 +13,19 @@ func Test_validateEmail(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{},
-		{},
-		{},
+		{
+			name: "test-1",
+			args: args{
+				file: "scripts/SimpleConcordPlaybook.yaml",
+				s:    "a@walmart.com",
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := validateOwner(tt.args.file, tt.args.s); (err != nil) != tt.wantErr {
-				t.Errorf("validateEmail() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("validateOwner() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
